03f82c295d942b1481aa4fa281d9f26a99fa00b6
mr-beckerleg <61328175+mr-beckerleg@users.noreply.github.com>
2023-07-25 10:17:15 -0500

    Create number_guess.py
    An exercise in loops and random integers…

---
+++ number_guess.py
@@ -0,0 +1,76 @@
+import random
+
+def user_inputs():
+    tries = int(input("Enter the number of tries you'd like available: "))
+    start = int(input("Enter the LOW number for the game: "))
+    stop = int(input("Enter the HIGH number for the game: "))
+    game = int(input(f'''Would you like to use 
+    
+    1) User Input
+    2) Linear Search
+    3) Binary Search
+    '''))
+
+    if game == 1:
+        guess_random_number(tries, start, stop)
+    elif game == 2:
+        guess_random_num_linear(tries, start, stop)
+    else:
+        guess_random_num_binary(tries, start, stop)     
+
+def guess_random_number(tries, start, stop):
+    target = random.randint(start, stop)
+    while tries != 0:
+        print("Remaing tries:",tries)
+        guess = int(input("Enter your guess: "))
+        if guess < start or guess > stop:
+            print("Choose a number between", start, "and", stop)
+        else:
+            if guess == target:
+                print("\nNice Guess! You got it!\n")
+                return
+            elif guess < target:
+                print("\nToo Low\n")
+            else:
+                print("\nToo High\n")
+            tries -= 1
+    print("\nFAILED!\nThe correct number was", target)
+
+def guess_random_num_linear(tries, start, stop):
+    target = random.randint(start, stop)
+    print(f'Target number: {target}')
+    for x in range(start, stop +1):
+        print(f"Program's Guess: {x}")
+        if x == target:
+            print("Success! The Program Guessed the Number!")
+            return
+        tries -= 1
+        if tries == 0:
+            print("Program Failed!")
+            return
+    
+def guess_random_num_binary(tries, start, stop):
+    target = random.randint(start, stop +1)
+    print(f'''Random number to find: {target}!''')
+    while tries != 0:
+        guess =(start + stop) //2
+        print(f"Program's Guess: {guess}")
+        if guess == target:
+            print("Success! The program guessed the Number!")
+            return
+        elif guess < target:
+            start = guess
+            print("Guessing Higher!")
+        else:
+            stop = guess
+            print("Guessing Lower!")
+        tries -= 1
+        if tries == 1:
+            guess = random.randint(start, stop)
+    print("Program Failed!")
+            
+'''     Test Driver     '''
+#guess_random_number(5, 0, 10)
+#guess_random_num_linear(5, 0, 10)
+#guess_random_num_binary(5, 0, 100)
+user_inputs()
